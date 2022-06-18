5a2ea984c7b6edd37bb5667b0a1566fbb0fa9df5
Hendra Kahar <mapalaisme@gmail.com>
2022-06-19 03:33:56 +0800

    edit commit

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import axios from 'axios';
 
 //default base URL / EndPoint API
-axios.defaults.baseURL = "https://panel.gorontalohosting.com";
+axios.defaults.baseURL = "https://dkp.boalemokab.go.id/gh-admin";
 
 //define a routes
 const routes = [
